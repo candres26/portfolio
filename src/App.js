0680823fcbd22b72932911144306708d17c57595
Carlos Andres Reyes Andrade <karlosareyes@gmail.com>
2022-06-05 23:00:34 -0500

    Testing changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>This is my application</h1> 
+        <h1>This is the porfolio of Andres Reyes</h1> 
         <a
           className="App-link"
           href="https://reactjs.org"
