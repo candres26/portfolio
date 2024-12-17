b4791996397d0b648583c802a68bee89d8d39d36
Carlos Andres Reyes <karlosareyes@gmail.com>
2024-12-17 16:35:42 -0500

    Fix: ajustar la configuracion base de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://candres26.github.io',
-  base: 'portfolio',
+  base: '/portfolio',
   integrations: [tailwind()]
 });
